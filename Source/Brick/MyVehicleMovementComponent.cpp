7868988ba9545a594c5f4af6ed2956ef4c639ab8
Rui <xrjervis@gmail.com>
2019-10-23 01:21:30 -0700

    Decide not to use unreal physics for vehicles

--- Source/Brick/MyVehicleMovementComponent.cpp
+++ Source/Brick/MyVehicleMovementComponent.cpp
@@ -6,24 +6,24 @@
 
 void UMyVehicleMovementComponent::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
-	if (UpdatedComponent->IsSimulatingPhysics()) {
-		UpdatedPrimitive->AddForce(TractionForce);
-		UpdatedPrimitive->SetPhysicsLinearVelocity(UpdatedPrimitive->GetPhysicsLinearVelocity().GetClampedToMaxSize(2000.f));
-		TractionForce = FVector::ZeroVector;
-	}
+	static float cosValue = 0.f;
+	cosValue += DeltaTime * 5.f;
+	UpdatedComponent->SetWorldLocation(FVector(0.f, 0.f, 102.f + FMath::Cos(cosValue)));
 }
 
 void UMyVehicleMovementComponent::Accelerate(float value)
 {
-	TractionForce += FVector(UpdatedComponent->GetForwardVector() * 2000.f);
+	TractionForce += FVector(UpdatedComponent->GetForwardVector() * 2000.f * value);
 }
 
 void UMyVehicleMovementComponent::Brake(float value)
 {
-	TractionForce += FVector(-UpdatedComponent->GetForwardVector() * 2000.f);
+	TractionForce += FVector(-UpdatedComponent->GetForwardVector() * 2000.f * value);
 }
 
 void UMyVehicleMovementComponent::SteerRight(float value)
 {
-
+	FVector CurrentVelocity = UpdatedPrimitive->GetPhysicsLinearVelocity();
+	CurrentVelocity.RotateAngleAxis(45.f * value, UpdatedPrimitive->GetUpVector());
+	UpdatedPrimitive->SetPhysicsLinearVelocity(CurrentVelocity);
 }
--- Source/Brick/MyVehicleMovementComponent.h
+++ Source/Brick/MyVehicleMovementComponent.h
@@ -22,4 +22,6 @@ public:
 
 private:
 	FVector TractionForce;
+	FVector SteerTorque;
+	float MaxSpeed = 2000.f;
 };
