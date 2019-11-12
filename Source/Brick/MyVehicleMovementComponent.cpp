bce7cf6472a7cfb2d40c8ce6b0a0e60b5a8a7985
Rui <xrjervis@gmail.com>
2019-11-12 01:10:08 -0800

    Add new car model; Add Steering angle; Wheel rotates;

--- Source/Brick/MyVehicleMovementComponent.cpp
+++ Source/Brick/MyVehicleMovementComponent.cpp
@@ -8,139 +8,78 @@
 #include "Engine/World.h"
 #include "Kismet/GameplayStatics.h"
 #include "Math/NumericLimits.h"
+#include "Misc/AssertionMacros.h"
+#include "SuspensionComponent.h"
+
+UMyVehicleMovementComponent::UMyVehicleMovementComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UMyVehicleMovementComponent::BeginPlay()
+{
+	TArray<USceneComponent*> ChildrenComponents;
+	GetOwner()->GetRootComponent()->GetChildrenComponents(false, ChildrenComponents);
+
+	for (auto Child : ChildrenComponents) {
+		if (Child->GetFName() == TEXT("Suspension_FL")) {
+			Suspension_FL = Cast<USuspensionComponent>(Child);
+		}
+		else if (Child->GetFName() == TEXT("Suspension_FR")) {
+			Suspension_FR = Cast<USuspensionComponent>(Child);
+		}
+		else if (Child->GetFName() == TEXT("Suspension_RL")) {
+			Suspension_RL = Cast<USuspensionComponent>(Child);
+		}
+		else if (Child->GetFName() == TEXT("Suspension_RR")) {
+			Suspension_RR = Cast<USuspensionComponent>(Child);
+		}
+	}
+
+	check(Suspension_FL);
+	check(Suspension_FR);
+	check(Suspension_RL);
+	check(Suspension_RR);
+}
 
 void UMyVehicleMovementComponent::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
-	FVector Force_long(0.f);
-
-	// Get old velocity from last frame
-	FVector OldVelocity = UpdatedComponent->ComponentVelocity;
-	float OldSpeed = OldVelocity.Size();
-
-	// Fluid Mechanics: C_drag = 0.5f * C_d * Area * rho
-	// C_d depends on the shape of the car and determined via wind tunnel test. Approximate value for a Corvette: 0.3
-	// Air density (rho) is 1.29 kg/m3
-	// frontal area is approx 2.2 m2
-	constexpr float C_drag = 0.5f * 2.f * 5.f * 1.29f;
-	FVector Force_drag = -C_drag * OldVelocity * OldSpeed;
-
-	// Rolling resistance, approx. 30 times C_drag
-	constexpr float C_rr = 30.f * C_drag;
-	FVector Force_rr = -C_rr * OldVelocity;
-
-	// Calculate engine power and traction force
-	if (AccelerateValue > 0.f) {
-		EnginePower += AccelerateValue * DeltaTime * 1500.f;
+	if (FMath::Abs(InputAxisValueY) > SMALL_NUMBER) {
+		SteeringAngle += InputAxisValueY * 45.f * DeltaTime;
+		SteeringAngle = FMath::Clamp(SteeringAngle, MinSteeringAngle, MaxSteeringAngle);
 	}
 	else {
-		EnginePower -= DeltaTime * 4000.f;
+		SteeringAngle *= 0.9f;
 	}
-	EnginePower = FMath::Clamp(EnginePower, 0.f, MaxEnginePower);
-	FVector Force_traction = UpdatedComponent->GetForwardVector() * EnginePower;
 
-	FString DebugMsg = FString::Printf(TEXT("Engine power: %f"), EnginePower);
-	GEngine->AddOnScreenDebugMessage(-1, 0.0f, FColor::Cyan, DebugMsg);
-	DebugMsg = FString::Printf(TEXT("Speed: %f"), OldSpeed);
-	GEngine->AddOnScreenDebugMessage(-1, 0.0f, FColor::Cyan, DebugMsg);
+	//X-Roll, Y-Pitch, Z-Yaw
+	Suspension_FL->SetRelativeRotation(FQuat::MakeFromEuler(FVector(0.f, 0.f, SteeringAngle)));
+	Suspension_FR->SetRelativeRotation(FQuat::MakeFromEuler(FVector(0.f, 0.f, SteeringAngle)));
+
+	if (GEngine) {
+		GEngine->AddOnScreenDebugMessage(-1, 0.0f, FColor::Blue, FString::Printf(TEXT("SteeringAngle: %.2f"), SteeringAngle));
+	}
+}
+
+void UMyVehicleMovementComponent::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
+{
 	
-	// Calculate braking force
-	FVector Force_braking(0.f);
-	float C_braking = 8000.f;
-
-	if (BrakeValue > 0.f) {
-		// Do not apply braking force if speed is reduced to zero
-		if (OldSpeed > 0.f) {
-			Force_braking = -UpdatedComponent->GetForwardVector() * C_braking;
-			Force_long += Force_braking;
-		}
-	}
-	else {
-		Force_long += Force_traction;
-	}
-	Force_long += Force_drag + Force_rr;
-
-	float Mass = 1000.f; //kg
-
-	FVector Acceleration = Force_long / Mass;
-
-	FVector NewVelocity = OldVelocity + Acceleration * DeltaTime;
-
-	// Prevent velocity increasing backwards
-	if (BrakeValue > 0.f && (FVector::DotProduct(OldVelocity, NewVelocity) < 0.f)) {
-		NewVelocity = FVector::ZeroVector;
-	}
-
-	// Steering
-	// Calculate angular velocity
-	const float FrontRearWheelDist = 3.2f; // meters
-	float SteerDegrees = 30.f * SteerValue;
-	float SteerRadius = FrontRearWheelDist / FMath::Sin(FMath::DegreesToRadians(SteerDegrees));
-	float AngularSpeed = FMath::RadiansToDegrees(OldSpeed / SteerRadius);
-
-	// Rotates vehicle's body
-	UpdatedPrimitive->AddWorldRotation(FQuat::MakeFromEuler(FVector::UpVector * AngularSpeed * DeltaTime));
-
-	// Eliminate lateral velocity
-	FVector LateralVelocity = NewVelocity.ProjectOnToNormal(UpdatedPrimitive->GetRightVector());
-	NewVelocity -= LateralVelocity;
-
-	FVector DeltaMove = NewVelocity * DeltaTime * 100.f; //times 100 m -> cm
-
-	FHitResult Hit(1.0f);
-	SafeMoveUpdatedComponent(DeltaMove, UpdatedComponent->GetComponentRotation(), true, Hit);
-
-	if (Hit.IsValidBlockingHit()) {
-		NewVelocity = NewVelocity - 1.2f * NewVelocity.ProjectOnToNormal(Hit.Normal);
-		DeltaMove = DeltaMove - DeltaMove.ProjectOnToNormal(Hit.Normal);
-
-		// Handles the calculations and physics involved with sliding smoothly along collision surfaces like walls and ramps
-		// when a move results in a collision, rather than simply stopping in place and sticking to the wall or ramp
-		SlideAlongSurface(DeltaMove, 1.f - Hit.Time, Hit.Normal, Hit);
-	}
-
-	Velocity = NewVelocity;
-	UpdateComponentVelocity();
-	// -----------------------------------------------------------------------------
-
-// 	FHitResult Hit;
-// 	FCollisionQueryParams CollisionParams;
-// 
-// 	// Raycast
-// 	FVector Start = UpdatedComponent->GetComponentLocation();
-// 	FVector End = Start - UpdatedComponent->GetUpVector() * 2000.f;
-// 	DrawDebugLine(GetWorld(), Start, End, FColor::Green, false, -1.f, 0, 1.f);
-// 
-// 	FVector CurrentUpVector = UpdatedComponent->GetUpVector();
-// 
-// 	if (GetWorld()->LineTraceSingleByChannel(Hit, Start, End, ECC_Visibility, CollisionParams)) {
-// 		AlignmentNormal = Hit.ImpactNormal;
-// 		float LocX = UpdatedComponent->GetComponentLocation().X;
-// 		float LocY = UpdatedComponent->GetComponentLocation().Y;
-// 		UpdatedComponent->SetWorldLocation(Hit.Location + CurrentUpVector * (100.f + 10.f * FMath::Cos(5.f * UGameplayStatics::GetRealTimeSeconds(GetWorld()))));
-// 	}
-// 	else {
-// 		AlignmentNormal = FVector(0.f, 0.f, 1.f);
-// 	}
-// 
-// 	// Up Vector
-// 	DrawDebugLine(GetWorld(), Start, Start + CurrentUpVector * 100.f, FColor::Yellow, false, -1.f, 0, 2.f);
-// 
-// 	float AlignmentDiff = 1.f - FVector::DotProduct(CurrentUpVector.GetSafeNormal(), AlignmentNormal.GetSafeNormal());
-// 	FVector TorqueAxis = FVector::CrossProduct(AlignmentNormal, CurrentUpVector).GetSafeNormal();
-// 	UpdatedPrimitive->AddWorldRotation(FQuat::MakeFromEuler(TorqueAxis * DeltaTime * AlignmentDiff * 1000.f));
 }
 
 void UMyVehicleMovementComponent::Accelerate(float value)
 {
-	AccelerateValue = value;
 }
 
 void UMyVehicleMovementComponent::Brake(float value)
 {
-	BrakeValue = value;
 }
 
-void UMyVehicleMovementComponent::SteerRight(float value)
+void UMyVehicleMovementComponent::MoveForward(float AxisValue)
 {
-	SteerValue = value;
+	InputAxisValueX = AxisValue;
+}
+
+void UMyVehicleMovementComponent::SteerRight(float AxisValue)
+{
+	InputAxisValueY = AxisValue;
 }
--- Source/Brick/MyVehicleMovementComponent.h
+++ Source/Brick/MyVehicleMovementComponent.h
@@ -6,32 +6,43 @@
 #include "GameFramework/PawnMovementComponent.h"
 #include "MyVehicleMovementComponent.generated.h"
 
-UCLASS(Blueprintable)
+class USuspensionComponent;
+
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class BRICK_API UMyVehicleMovementComponent : public UPawnMovementComponent
 {
 	GENERATED_BODY()
 
 public:
+	UMyVehicleMovementComponent();
+
+	virtual void BeginPlay() override;
+
 	virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
+	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
+	
 	void Accelerate(float value); // [0.f, 1.f]
 	void Brake(float value); //[0.f, 1.f]
-	void SteerRight(float value); //[-1.f, 1.f]
+
+	UFUNCTION(BlueprintCallable, Category = "VehicleMovement")
+	void MoveForward(float AxisValue); //[-1.f, 1.f]
+	UFUNCTION(BlueprintCallable, Category = "VehicleMovement")
+	void SteerRight(float AxisValue); //[-1.f, 1.f]
 
 public:
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Vehicle")
-	float EnginePower = 0.f;
-
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Vehicle")
-	float MaxEnginePower = 4000.f;
+	UPROPERTY(EditAnywhere, Category = "VehicleMovement")
+	float MinSteeringAngle = -45.f;
+	UPROPERTY(EditAnywhere, Category = "VehicleMovement")
+	float MaxSteeringAngle = 45.f;
 
 private:
-	FVector TractionForce;
-	FVector SteerTorque;
+	float InputAxisValueX = 0.f;
+	float InputAxisValueY = 0.f;
+	float SteeringAngle = 0.f;
 
-	FVector AlignmentNormal;
-
-	float AccelerateValue = 0.f;
-	float BrakeValue = 0.f;
-	float SteerValue = 0.f;
+	USuspensionComponent* Suspension_FL = nullptr;
+	USuspensionComponent* Suspension_FR = nullptr;
+	USuspensionComponent* Suspension_RL = nullptr;
+	USuspensionComponent* Suspension_RR = nullptr;
 };
